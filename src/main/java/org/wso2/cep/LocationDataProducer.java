15a372b7e369789b840000686e0f548f5e0dfa69
heshaniot <heshaniot@gmail.com>
2014-09-29 09:03:12 +0530

    Formatting

--- src/main/java/org/wso2/cep/LocationDataProducer.java
+++ src/main/java/org/wso2/cep/LocationDataProducer.java
@@ -1,62 +1,59 @@
 package org.wso2.cep;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Logger;
-
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.SystemDefaultHttpClient;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LocationDataProducer {
 
-    public Logger log = Logger.getLogger(LocationDataProducer.class);
-
     public static void main(String[] args) {
-		String url = args[0];
+        String url = args[0];
         HttpClient httpClient = new SystemDefaultHttpClient();
 
-		try {
-			HttpPost method = new HttpPost(url);
+        try {
+            HttpPost method = new HttpPost(url);
 
-			BufferedReader br = new BufferedReader(new FileReader("resources/input.csv"));
+            BufferedReader br = new BufferedReader(new FileReader("resources/input.csv"));
 
-			String line = "";
-			List<GeoJsonPointFeature> locationList = new ArrayList<GeoJsonPointFeature>();
-			while ((line = br.readLine()) != null) {
-				String[] location = line.split(",");
-				String deviceId = location[0];
-				String timeStamp = location[1];
-				double longitude = Double.parseDouble(location[2]);
-				double lattitude = Double.parseDouble(location[3]);
+            String line = "";
+            List<GeoJsonPointFeature> locationList = new ArrayList<GeoJsonPointFeature>();
+            while ((line = br.readLine()) != null) {
+                String[] location = line.split(",");
+                String deviceId = location[0];
+                String timeStamp = location[1];
+                double longitude = Double.parseDouble(location[2]);
+                double latitude = Double.parseDouble(location[3]);
 
-				GeoJsonPointFeature geoJsonPointFeature =
-				                                          new GeoJsonPointFeature(deviceId,
-				                                                                  timeStamp);
-				GeoJsonPoint geoJsonPoint = new GeoJsonPoint(longitude, lattitude);
-				geoJsonPointFeature.setGeometry(geoJsonPoint);
-				locationList.add(geoJsonPointFeature);
-			}
+                GeoJsonPointFeature geoJsonPointFeature =
+                        new GeoJsonPointFeature(deviceId,
+                                timeStamp);
+                GeoJsonPoint geoJsonPoint = new GeoJsonPoint(longitude, latitude);
+                geoJsonPointFeature.setGeometry(geoJsonPoint);
+                locationList.add(geoJsonPointFeature);
+            }
 
-			if (httpClient != null) {
-				int i = 0;
-				for (GeoJsonPointFeature geoJsonPointFeature : locationList) {
-					StringEntity gpsEntity = new StringEntity(geoJsonPointFeature.getAsGeoJson());
-					gpsEntity.setContentType("application/json");
-					method.setEntity(gpsEntity);
+            if (httpClient != null) {
+                int i = 0;
+                for (GeoJsonPointFeature geoJsonPointFeature : locationList) {
+                    StringEntity gpsEntity = new StringEntity(geoJsonPointFeature.getAsGeoJson());
+                    gpsEntity.setContentType("application/json");
+                    method.setEntity(gpsEntity);
 
-					httpClient.execute(method).getEntity().getContent().close();
-					System.out.println("Sent event no :" + i++ + " " +
-					                   geoJsonPointFeature.getAsGeoJson());
+                    httpClient.execute(method).getEntity().getContent().close();
+                    System.out.println("Sent event no :" + i++ + " " +
+                            geoJsonPointFeature.getAsGeoJson());
 
-				}
-				Thread.sleep(500);
-			}
-		} catch (Throwable t) {
-			t.printStackTrace();
-		}
-	}
+                }
+                Thread.sleep(500);
+            }
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
 }
